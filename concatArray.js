b6e5516a665d2ce52215291884150e971153dd6b
Tanyathip <Tanyathip.aoongern@mail.kmutt.ac.th>
2022-09-15 00:19:37 +0700

    Homework

--- concatArray.js
+++ concatArray.js
@@ -1,6 +1,26 @@
 const { template } = require('@babel/core')
 
+
 function concatArray(array1, array2) {
-  //code here
+  if((array1 == null || undefined) && (array2 == null||undefined)){
+      return undefined
+      // console.log(undefined)
+  }
+  else if ((array1 == null||undefined) && (array2 != null||undefined)){
+      // console.log(array2)
+      return array2
+  }
+  else if ((array2 == null||undefined) && (array1 != null||undefined)){
+      // console.log(array1)
+      return array1
+  }
+  else{
+      
+      arr = array1.concat(array2)
+      // console.log(arr)
+      return arr
+  }
+  
 }
+
 module.exports = concatArray
